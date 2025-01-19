395b2f7de9a4a1cef512c1c58632a38dbee47355
Judszn <jude.niitetteh@gmail.com>
2025-01-19 21:04:15 +0000

    I need more time

--- PASSCODE/script.js
+++ PASSCODE/script.js
@@ -3,6 +3,8 @@ const modal = document.getElementById("modal");
 const inputBox = document.getElementById("passwordInput");
 const linkInput = document.getElementById("linkInput");
 const generatePassword = document.getElementById("generate");
+const saveButton = document.getElementById("saveButton");
+const historyList = document.getElementById("list");
 
 function displayModal() {
   modal.style.display = "flex";
@@ -13,25 +15,65 @@ generateButton.addEventListener("click", displayModal);
 const chars = 12;
 
 const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
 const lowerCase = "abcdefghijklmnopqrstuvwxyz";
 const number = "0123456789";
 const symbol = "@#$%^&*()_+~|{}[]></-=";
 
 const allChars = upperCase + lowerCase + number + symbol;
-console.log(allChars);
 
-const createPassword = function GenChars() {
+function createPassword() {
   let password = "";
   password += upperCase[Math.floor(Math.random() * upperCase.length)];
   password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
-  number += number[Math.floor(Math.random() * number.length)];
-  symbol += symbol[Math.floor(Math.random() * symbol.length)];
+  password += number[Math.floor(Math.random() * number.length)];
+  password += symbol[Math.floor(Math.random() * symbol.length)];
 
-  while (length > password.length) {
+  while (chars > password.length) {
     password += allChars[Math.floor(Math.random() * allChars.length)];
   }
   inputBox.value = password;
-};
+  saveButton.style.display = "block";
+
+  // alert("Add the link for your password");
+}
+
+function copyPassword() {
+  navigator.clipboard
+    .writeText(inputBox.value)
+    .then(() => {})
+    .catch((err) => {
+      console.error("Failed to copy text: ", err);
+    });
+
+  if (!(inputBox.value = "")) {
+    alert("Password copied to clipboard!");
+    console.log("Gotcha");
+  } else {
+    console.log("Okay sure");
+  }
+}
 
 generatePassword.addEventListener("click", createPassword);
+
+function saveLink() {
+  const linkText = linkInput.value;
+  const link = document.createElement("list");
+  const password = inputBox.value;
+  const passwordText = document.createElement("p");
+  passwordText.innerHTML = password;
+  link.innerHTML = linkText;
+  link.classList.add("list-item");
+  historyList.appendChild(link);
+
+  link.addEventListener("click", showAccount);
+  saveButton.style.display = "none";
+  inputBox.value = "";
+  linkInput.value = "";
+}
+
+const showAccount = () => {
+  inputBox.value = password;
+  linkInput.value = link;
+};
+
+saveButton.addEventListener("click", saveLink);
